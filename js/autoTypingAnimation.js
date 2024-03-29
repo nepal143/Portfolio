5aa2b0842184f7faaa6e383fb686c07762d5648e
nepla143 <nepalsss007@gmail.com>
2024-03-29 16:08:33 +0530

    added portfolio

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Nepal Singh</span> As a seasoned web developer, I leverage a versatile toolkit of technologies to craft dynamic and responsive websites. My expertise spans HTML, CSS, and JavaScript for front-end development, while Node.js and Express.js power robust back-end functionalities. I specialize in modern frameworks like React.js for building interactive user interfaces and Bootstrap and Tailwind for efficient and visually appealing designs. With MongoDB as my preferred database solution, I ensure seamless data management and scalability for web applications, delivering optimal performance and user experience.<br> <br> In the realm of game development, I harness the power of Unity and C# to create immersive and engaging gaming experiences. From designing captivating gameplay mechanics to implementing stunning 3D graphics and animations, I bring creativity and technical expertise to every project. By integrating cutting-edge technologies like AR and VR, I push the boundaries of gaming innovation, delivering memorable experiences across various platforms. My passion for game development drives me to stay updated with the latest trends and techniques, ensuring that my games stand out in the competitive gaming market."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
